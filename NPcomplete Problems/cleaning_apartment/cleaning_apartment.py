f46e650127479a8b2f21b4db1d96d41c064addde
Tim Mason <millionsofbillions@gmail.com>
2017-04-22 19:02:57 -0500

    cleaningApartment savePoint

--- NPcomplete Problems/cleaning_apartment/cleaning_apartment.py
+++ NPcomplete Problems/cleaning_apartment/cleaning_apartment.py
@@ -1,4 +1,7 @@
 # python3
+import sys
+
+
 n, m = map(int, input().split())
 edges = [ list(map(int, input().split())) for i in range(m) ]
 
@@ -8,8 +11,13 @@ class Position:
 		self.vertex = vertex
 		self.pos = pos
 
-def printClause(clause):
-	print(clause)
+def printClauses(clauses,numVar):
+	print(str(len(clauses)) + ' ' + str(numVar))
+	for clause in clauses:
+		for i in clause:
+			sys.stdout.write(str(i))
+			sys.stdout.write(' ')
+		sys.stdout.write('0 \n')
 
 def printEquisatisfiableSatFormula():
 
@@ -22,21 +30,56 @@ def printEquisatisfiableSatFormula():
 		for j in range(n):
 			variables.append(Position(varCount,i,j))
 			varCount += 1
-
-	#enforce that every room must be visited
-	for i in range(n):
+	clauses = []
+	for i in range(n):  #loop over rooms
+	
+		#enforce that every room must be visited
 		#get all the variables corresponding to this position and ensure one of them is true
 		vars = [j.varNum for j in variables if j.pos == i]
 		#create an or clause of these variables and add it
 		clause = []
 		for j in vars:
 			clause.append(j)
-		printClause(clause)
+		clauses.append(clause)
+		
+		#enforce that no room can be visited more than once
+		#loop over positions for this room and ensure it has no more that 1 position
+		for j in range(n):
+			for k in range(n):
+				#for each pair of position variable ensure that at most of them is true
+				if j != k:
+					clause = []
+					clause.append(-vars[j])
+					clause.append(vars[k])
+					clauses.append(clause)
+					
+	#use edges to determine which transitions are not allowed
+	#for example, if no edge exists between room 3 and room4, we must insert a condition that
+	#room3 cannot be adjacent to room 4  (room3 in position i implies room 4 cannot be in position 5 or 3)
 	
-	#enforce that each room can only be visited once
-	
-	#enforce that vertices with adjacent positions must be connected on the graph through an edge
+	#loop through all possible connections
+	for i in range(n):
+		for j in range(n):
+			if i != j:
+				#determine if edge exists
+				edgeExists = False
+				for edge in edges:
+					if i == edge[0] and j == edge[1]:
+						edgeExists = True
+					if i == edge[1] and j == edge[0]:
+						edgeExists = True
+				print(edgeExists)
+				#if no edge, add a clause excluding this connection for each step
+				if not edgeExists:
+					for step in range(2,n):
+						#get variable for these edges and these steps
+						var1 =  [k.varNum for k in variables if k.vertex == i and k.pos == step][0]
+						var2 =  [k.varNum for k in variables if k.vertex == j and k.pos == step-1][0]
+						clause = []
+						clause.append(-var1)
+						clause.append(-var2)
+						clauses.append(clause)
 
-
+	#printClauses(clauses,len(variables))
 
 printEquisatisfiableSatFormula()
